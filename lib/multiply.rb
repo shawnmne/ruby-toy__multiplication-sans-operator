875cb1cad82a3a5845b605eb247d0ee5fbb992dc
shawnmne <shawnmne@hotmail.com>
2016-02-25 03:47:16 +0000

    done

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,10 @@
 def multiply(num1, num2)
+    i = 0                   #counter for loop   
+    total = 0               #set original total to 0
+    
+    while i < num1 do       #loop through num1 times
+        total = total + num2  #add num2 to the total each time through the loop
+        i += 1              #increment counter
+    end
+    return total            #return total
 end
